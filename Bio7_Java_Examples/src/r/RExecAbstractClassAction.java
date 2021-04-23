f4e0b5ebfe544afc319a7015b5d97bbc3126ed9c
Bio7 <marcel.au@web.de>
2021-04-23 16:36:41 +0200

    Added Java R execution interface actions.

---
+++ Bio7_Java_Examples/src/r/RExecAbstractClassAction.java
@@ -0,0 +1,39 @@
+package r;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.rosuda.REngine.Rserve.RConnection;
+import org.rosuda.REngine.Rserve.RserveException;
+import com.eco.bio7.rbridge.ExecuteRScript;
+import com.eco.bio7.rbridge.RScriptExecuter;
+
+/*An example to execute R code running in a job using Raw String Literals with Java 15!*/
+
+public class RExecAbstractClassAction {
+	String rCommand = """
+			print(runif(100))
+			""";
+	public RExecAbstractClassAction() {
+		/*
+		 * The ExecuteRScript class executes the 'evaluate' method of a given RScriptExecuterInterface!
+		 * The abstract RScriptExecuter class implements the interface so we can override
+		 * the evaluate method of that class which is called from the ExecuteRScript class!
+		 */
+		new ExecuteRScript(new RScriptExecuter() {
+			public void evaluate(RConnection con, IProgressMonitor monitor) {
+				try {			
+					con.eval(rCommand);
+					monitor.worked(1);
+
+				} catch (RserveException e) {
+					e.printStackTrace();
+				}
+			}
+
+		},2);
+
+	}
+    
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ Bio7_Java_Examples/src/r/RExecInterfaceAction.java
@@ -0,0 +1,39 @@
+package r;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.rosuda.REngine.Rserve.RConnection;
+import org.rosuda.REngine.Rserve.RserveException;
+import com.eco.bio7.rbridge.ExecuteRScript;
+import com.eco.bio7.rbridge.RScriptExecuterInterface;
+
+/*An example to execute R code running in a job using Raw String Literals with Java 15!*/
+public class RExecInterfaceAction implements RScriptExecuterInterface {
+	String rCommand = """
+			try(plot(runif(100)))
+			""";
+
+	public RExecInterfaceAction() {
+		/*
+		 * We create a new instance of the ExecuteRScript class which executes the
+		 * evaluate method of this given RScriptExecuterInterface interface!
+		 */
+		new ExecuteRScript(this, 1);
+	}
+
+	/* We declare the interface method! */
+	public void evaluate(RConnection con, IProgressMonitor monitor) {
+
+		try {
+			con.eval(rCommand);
+			monitor.worked(1);
+		} catch (RserveException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// Main method to avoid warning!
+	}
+
+}
